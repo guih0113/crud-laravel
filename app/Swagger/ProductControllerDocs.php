e1fa928e833b104e25161f8782e1d94683dde096
guih0113 <gui.henriquess13@gmail.com>
2026-09-24 21:59:53 -0300

    chore: add Swagger documentation dependencies

---
+++ app/Swagger/ProductControllerDocs.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Swagger;
+
+class ProductControllerDocs
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
+
+// Rodar "php artisan l5-swagger:generate" para gerar a documentação do Swagger
